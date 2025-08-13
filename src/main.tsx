00de111e6ce187809bb7604dc88b41753df6901a
Lee Spence <leespencedev@gmail.com>
2025-08-12 19:39:26 -0700

    feat: add vercel analyics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import BitcoinTracker from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <BitcoinTracker />
+      <Analytics />
     </Router>
   </React.StrictMode>,
 );
